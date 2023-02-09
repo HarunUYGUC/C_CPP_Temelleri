bf59eacbad1648f223b70564db81b25a42db3aa3
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-09 22:11:59 +0300

    C'de int Değişkeni

---
+++ C_C++_Temelleri/degiskenler.c
@@ -0,0 +1,61 @@
+/*
+#include <stdio.h>
+
+main()
+{
+	int sayi1 = 10, sayi3, sayi4;
+	int sayi2;
+	sayi2 = 20;
+	printf("%d", sayi1);
+}
+*/
+
+/*
+#include <stdio.h>
+
+main()
+{
+	int sayi1 = 10, sayi3, sayi4;
+	int sayi2;
+	sayi2 = 20;
+	printf("%d ve %d", sayi1, sayi2);
+}
+*/
+
+/*
+#include <stdio.h>
+
+main()
+{
+	int sayi1 = 10, sayi3, sayi4;
+	int sayi2;
+	sayi2 = 20;
+	printf("%d ve %d", sayi1, sayi2);
+	printf("Merhaba");
+}
+*/
+
+/*
+#include <stdio.h>
+
+main()
+{
+	int sayi1 = 10, sayi3, sayi4;
+	int sayi2;
+	sayi2 = 20;
+	printf("%d ve %d \n", sayi1, sayi2); // n komutu önemli!!!
+	printf("Merhaba");
+}
+*/
+
+/*
+#include <stdio.h>
+
+main()
+{
+	int sayi1 = 10, sayi3, sayi4;
+	sayi2 = 20  // Böyle bir tanýmlama yapýlamaz.
+	printf("%d ve %d \n", sayi1, sayi2);
+	printf("Merhaba");
+}
+*/
