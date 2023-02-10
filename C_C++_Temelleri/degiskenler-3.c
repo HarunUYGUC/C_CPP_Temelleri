83f6981483be5611c0eadb90d826e54bed5a3333
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-10 22:44:13 +0300

    C | float, double veri tipi

---
+++ C_C++_Temelleri/degiskenler-3.c
@@ -0,0 +1,54 @@
+/*
+#include <stdio.h>
+
+main()
+{
+	float sonuc;
+	sonuc = 11 / 5;
+	printf("%f", sonuc);
+}
+*/
+
+/*
+#include <stdio.h>
+
+main()
+{
+	float sonuc;
+	sonuc = 11.0 / 5;
+	printf("%f", sonuc);
+}
+*/
+
+/*
+#include <stdio.h>
+
+main()
+{
+	double sonuc;
+	sonuc = 11.0 / 5;
+	printf("%lf", sonuc);
+}
+*/
+
+/*
+#include <stdio.h>
+
+main()
+{
+	double sonuc;
+	sonuc = 11.0 / 5;
+	printf("%.3lf", sonuc);
+}
+*/
+
+/*
+#include <stdio.h>
+
+main()
+{
+	float sonuc;
+	sonuc = 11.0 / 5;
+	printf("%.3f", sonuc);
+}
+*/
