ed0faa1a6d0fdff3bd399f9535b10f7c4664d73d
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-10 12:57:22 +0300

    C++ | string, char veri tipleri

---
+++ C_C++_Temelleri/degiskenler-2.cpp
@@ -0,0 +1,57 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	char k1 = 'A';  // char veri tipi ' ' tek týrnak ile gösterilir. string veri tipi " " çift týrnak ile gösterilir.
+	cout << k1;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	char k1 = 'Abla';  // Hatalý sonuç. char veri tipi birden fazla karakter alamaz.
+	cout << k1;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	char k1 = 'A'; 
+	cout << int(k1);
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	char k1 = 65; 
+	cout << k1;
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	string metin = "Harun Uyguç";
+	cout << metin;
+}
