fbfbaeefb39e2eff2a9110404916d6b73146f845
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-10 13:35:13 +0300

    C | char veri tipi

---
+++ C_C++_Temelleri/degiskenler-2.c
@@ -0,0 +1,31 @@
+/*
+#include <stdio.h>
+
+main()
+{	
+	char k = 66;
+	printf("%c", k);
+}
+*/
+
+/*
+#include <stdio.h>
+
+main()
+{
+	char k = 'C';
+	printf("%d", k);
+}
+*/
+
+/*
+#include <stdio.h>
+#include <locale.h>
+
+main()
+{
+	setlocale(LC_ALL, "Turkish");
+	char k[20] = "Harun Uyguç";
+	printf("%s", k);
+}
+*/
