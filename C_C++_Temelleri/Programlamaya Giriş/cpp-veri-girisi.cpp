7e81dc34e101f9c48e52f1dcd95100e1db97e34c
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-12 16:51:51 +0300

    C++ | Veri Girişi

---
+++ C_C++_Temelleri/Programlamaya Giriş/cpp-veri-girisi.cpp
@@ -0,0 +1,35 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char k;
+	string metin;
+	int sayi1;
+	float sayi2;
+	double sayi3;
+
+	cout << "Bir karakter giriniz: ";
+	cin >> k;
+
+	cout << "Bir metin giriniz: ";
+	cin >> metin;
+
+	cout << "Bir tam sayý giriniz: ";
+	cin >> sayi1;
+
+	cout << "Float tipinde bir sayý giriniz: ";
+	cin >> sayi2;
+
+	cout << "Double tipinde bir sayý giriniz: ";
+	cin >> sayi3;
+
+	cout << k << endl;
+	cout << metin << endl;
+	cout << sayi1 << endl;
+	cout << sayi2 << endl;
+	cout << sayi3 << endl;
+	cout << &sayi1;
+}
+*/
