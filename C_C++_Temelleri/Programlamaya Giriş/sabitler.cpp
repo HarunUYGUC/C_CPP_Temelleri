4d356c9ef2e128b1bce1fb4ff31aaa5fa43061f6
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-12 17:13:43 +0300

    C & C++ | Sabitler

---
+++ C_C++_Temelleri/Programlamaya Giriş/sabitler.cpp
@@ -0,0 +1,33 @@
+/*
+#include <iostream>
+using namespace std;
+#define PI 3.14
+
+int main()
+{
+	float r, alan;
+
+	cout << "Yarýçapý giriniz: ";
+	cin >> r;
+
+	alan = PI * r * r;
+	cout << "Çemberin alaný = " << alan;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const float PI = 3.14;
+	float r, alan;
+
+	cout << "Yarýçapý giriniz: ";
+	cin >> r;
+
+	alan = PI * r * r;
+	cout << "Çemberin alaný = " << alan;
+}
+*/
