79d14a3cdbf82d82b67e03dfbf2150189f419981
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-12 13:36:36 +0300

    C | veri girişi

---
+++ C_C++_Temelleri/Programlamaya Giriş/c-veri-girisi.c
@@ -0,0 +1,34 @@
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+main()
+{
+	char k;
+	char metin[20];
+	int sayi1;
+	float sayi2;
+	double sayi3;
+
+	printf("Bir karakter giriniz: ");
+	scanf_s("%c", &k);
+
+	printf("Bir metin giriniz: ");
+	scanf("%s", &metin);
+
+	printf("Bir tam sayý giriniz: ");
+	scanf_s("%d", &sayi1);
+
+	printf("Float tipinde bir sayý giriniz: ");
+	scanf_s("%f", &sayi2);
+
+	printf("Double veri tipinde bir sayý giriniz: ");
+	scanf_s("%lf", &sayi3);
+
+	printf("%c\n", k);
+	printf("%s\n", metin);
+	printf("%d\n", sayi1);
+	printf("%f\n", sayi2);
+	printf("%lf", sayi3);
+}
+*/
