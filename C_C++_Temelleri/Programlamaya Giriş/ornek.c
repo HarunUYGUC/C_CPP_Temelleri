5b778ebfcf17d3c1fbcdf11756ca58aed3ca4cfc
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-13 15:37:09 +0300

    Örnekler

---
+++ C_C++_Temelleri/Programlamaya Giriş/ornek.c
@@ -0,0 +1,41 @@
+/*
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	float kisaKenar, uzunKenar, cevre, alan;
+
+	printf("Dikdörtgenin kýsa kenarýný giriniz: ");
+	scanf_s("%f", &kisaKenar);
+
+	printf("Dikdörtgenin uzun kenarýný giriniz: ");
+	scanf_s("%f", &uzunKenar);
+
+	cevre = 2 * (kisaKenar + uzunKenar);
+	alan = kisaKenar * uzunKenar;
+
+	printf("Dikdörtgenin çevresi = %f \n", cevre);
+	printf("Dikdörtgenin alaný = %f", alan);
+}
+*/
+
+/*
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#define PII 3.14
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	float r, alan;
+	
+	printf("Çemberin yarýçapýný giriniz: ");
+	scanf_s("%f", &r);
+
+	alan = PII * pow(r, 2);
+	printf("Çemberin alaný = %f", alan);
+}
+*/
---
+++ C_C++_Temelleri/Programlamaya Giriş/ornek.cpp
@@ -0,0 +1,41 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	setlocale(LC_ALL, "Turkish");
+	float kisaKenar, uzunKenar, cevre, alan;
+
+	cout << "Dikdörtgenin kýsa kenarýný giriniz: ";
+	cin >> kisaKenar;
+	
+	cout << "Dikdörtgenin uzun kenarýný giriniz: ";
+	cin >> uzunKenar;
+
+	cevre = 2 * (kisaKenar + uzunKenar);
+	alan = kisaKenar * uzunKenar;
+
+	cout << "Dikdörtgenin çevresi = " << cevre << endl;
+	cout << "Dikdörtgenin alaný = " << alan;
+}
+*/
+
+/*
+#include <iostream>
+#include <math.h>
+using namespace std;
+#define PI 3.14;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	float r, alan;
+
+	cout << "Çemberin yarýçapýný giriniz: ";
+	cin >> r;
+
+	alan = PI * pow(r, 2);
+	cout << "Çemberin alaný = " << alan;
+}
+*/
