653aa95ca57ac71ae87e9a7248133931fdf4c9d6
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-13 13:03:40 +0300

    Matematiksel Operatörler

---
+++ C_C++_Temelleri/Programlamaya Giriş/operatorler.cpp
@@ -0,0 +1,32 @@
+/*
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int b = 4;
+	int islem1, islem2, islem3, islem5;
+	float islem4, islem6, us, karekok;
+
+	islem1 = a + b;
+	islem2 = a - b;
+	islem3 = a * b;
+	islem4 = float(a) / b;
+	islem5 = a % b;
+	islem6 = a + b * 3;
+	us = pow(a, b);
+	karekok = sqrt(16);
+
+	cout << "Toplama = " << islem1 << endl;
+	cout << "Çýkarma = " << islem2 << endl;
+	cout << "Çarpma = " << islem3 << endl;
+	cout << "Bölme = " << islem4 << endl;
+	cout << "Mod Alma = " << islem5 << endl;
+	cout << "a + b * 3 = " << islem6 << endl;
+	cout << "Üs Alma = " << us << endl;
+	cout << "Üs Alma = " << a << "^" << b << " = " << us << endl;
+	cout << "Karekök= " << karekok;
+}
+*/
