1b339d0f4cb0107c61b47bc5506823a4310c8adb
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-15 21:21:29 +0300

    Aritmetiksel Atama Operatörleri

---
+++ C_C++_Temelleri/Programlamaya Giriş/aritmetiksel-atama-operatorleri.cpp
@@ -0,0 +1,28 @@
+/*
+Aritmetiksel atama operatörleri:
+
++= Toplayarak Atama
+-= Çýkararak Atama
+*= Çarparak Atama
+/= Bölerek Atama
+
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 20;
+	int b = 4;
+
+	a = a + 5;
+	cout << a << endl;
+	a += 5;
+	cout << a << endl; 
+
+	a /= b;
+	cout << a;
+}
+*/
