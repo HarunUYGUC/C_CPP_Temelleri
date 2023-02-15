38e096e946fef410f1c0b4297adecf2475f106b1
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-15 22:16:06 +0300

    Mantıksal Operatörler

---
+++ C_C++_Temelleri/Programlamaya Giriş/mantiksal-operatorler.cpp
@@ -0,0 +1,46 @@
+/*
+Mantýksal Operatörler:
+
+And (Ve) : && => Birden fazla karþýlaþtýrma ifadesinden hepsinin doðru olmasý sonucunda doðru sonucunu döndürür.
+Or (Veya) : || => Birden fazla karþýlaþtýrma ifadesinden birisinin doðru olmasý sonucunda doðru sonucunu döndürür. 
+Not (Deðil): ! => Ýþlemi tersine çevirir. Doðru ise yanlýþ, yanlýþ ise doðru yapar.
+
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int final = 65;
+	int ortalama = 80;
+
+	cout << (final > 60 && ortalama > 60);
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int final = 65;
+	int ortalama = 55;
+
+	cout << (final > 60 && ortalama > 60);
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	bool deger = 1;
+
+	cout << (!deger);
+}
+*/
