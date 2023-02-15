f6f0da6dda8cab7d2dbc0f676149588c908aaa74
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-15 21:37:15 +0300

    Karşılaştırma Operatörleri

---
+++ C_C++_Temelleri/Programlamaya Giriş/karsilastirma-operatorleri.cpp
@@ -0,0 +1,41 @@
+/*
+Matematiksel ifadeleri karþýlaþtýrmak için kullanýlan operatörlerdir. 
+Karþýlaþtýrma operatörlerinin sonucu her zaman 0 ve 1 idir.
+
+Karþýlaþtýrma ifadesinin sonucu 0 ise karþýlaþtýrma ifadesinin sonucunun yanlýþ olduðunu gösterir.
+Karþýlaþtýrma ifadesinin sonucu 1 ise karþýlaþtýrma ifadesinin sonucunun doðru olduðunu gösterir.
+
+== Eþit mi?
+> Büyük mü?
+< Küçük mü?
+>= Büyük veya Eþit mi?
+<= Küçük veya Eþit mi?
+!= Eþit Deðil mi?
+
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int b = 10;
+
+	cout << (a < b);
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int b = 10;
+
+	cout << (a == b);
+}
+*/
