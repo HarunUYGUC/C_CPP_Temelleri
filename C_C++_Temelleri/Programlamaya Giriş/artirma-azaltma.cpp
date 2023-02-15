fdf4ace7c6c44947afadbb3f99243b77cfb5142a
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-15 21:08:01 +0300

    Artırma & Azaltma

---
+++ C_C++_Temelleri/Programlamaya Giriş/artirma-azaltma.cpp
@@ -0,0 +1,90 @@
+/*
+Önden ve sondan arttırma ve azaltma operatörleri:
+
+degisken++ : Sonradan Artırma
+degisken-- : Sonradan Azaltma
+++degisken : Önceden Artırma
+--degisken : Önceden Azaltma
+
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	a = a + 1;
+	cout << a << endl;
+	a += 1;
+	cout << a << endl;
+	a++;
+	cout << a;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int b = 4;
+	int c;
+	
+	c = a++;
+	cout << c << endl;
+	cout << a;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int b = 4;
+	int c;
+
+	c = a++ + b;
+	cout << c << endl;
+	cout << a;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int b = 4;
+	int c;
+
+	c = ++a + b;
+	cout << c << endl;
+	cout << a;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int b = 4;
+	int c;
+
+	c = --a + b++;
+	cout << a << endl;
+	cout << b << endl;
+	cout << c;
+}
+*/
