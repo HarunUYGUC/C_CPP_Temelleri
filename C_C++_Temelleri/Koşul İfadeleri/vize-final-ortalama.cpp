8f65cab29826fecac62ed12e6ffaa2d74e884a18
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-16 13:36:55 +0300

    İç İçe (Nested) If - Else If - Else

---
+++ C_C++_Temelleri/Koşul İfadeleri/vize-final-ortalama.cpp
@@ -0,0 +1,35 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float ortalama;
+	int vize, final;
+
+	cout << "Vize notunu giriniz: ";
+	cin >> vize;
+	
+	cout << "Final notunu giriniz: ";
+	cin >> final;
+
+	if (vize < 0 || vize > 100 || final < 0 || final > 100)
+	{
+		cout << "Geçersiz not girdiniz.";
+	}
+	else
+	{
+		ortalama = vize * 0.3 + final * 0.7;
+		cout << "Ortalamanýz = " << ortalama << endl;
+
+		if (final >= 60 && ortalama >= 60)
+		{
+			cout << "Geçtiniz.";
+		}
+		else
+		{
+			cout << "Kaldýnýz.";
+		}
+	}
+}
+*/
