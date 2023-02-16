5bc975847831874e927198194973b482cc8c15ba
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-16 15:43:58 +0300

    Switch - Case

---
+++ C_C++_Temelleri/Koşul İfadeleri/switch-case.cpp
@@ -0,0 +1,86 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+
+	switch (degisken)
+	{
+	case d1: 
+	{
+		islemler; 
+		break;
+	}
+	case d2: 
+	{
+		islemler; 
+		break;
+	}	
+	case d3: 
+	{
+		islemler; 
+		break;
+	}
+	default:
+	{
+		cout << "Hatalý iþlem seçtiniz.";
+	}
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int sayi1, sayi2, islem;
+
+	cout << "1. Sayýyý Giriniz: ";
+	cin >> sayi1;
+
+	cout << "2. Sayýyý Giriniz: ";
+	cin >> sayi2;
+
+	cout << endl << endl;
+	cout << "**** Ýþlem Seçiniz ****" << endl;
+
+	cout << "1. Toplama" << endl;
+	cout << "2. Çýkarma" << endl;
+	cout << "3. Çarpma" << endl;
+	cout << "4. Bölme" << endl;
+
+	cin >> islem;
+
+	switch (islem)
+	{
+	case 1: 
+	{
+		cout << sayi1 << " + " << sayi2 << " = " << sayi1 + sayi2;
+		break;
+	}
+	case 2:
+	{
+		cout << sayi1 << " - " << sayi2 << " = " << sayi1 - sayi2;
+		break;
+	}
+	case 3:
+	{
+		cout << sayi1 << " * " << sayi2 << " = " << sayi1 * sayi2;
+		break;
+	}
+	case 4:
+	{
+		cout << sayi1 << " / " << sayi2 << " = " << float(sayi1) / sayi2;
+		break;
+	}
+	default:
+	{
+		cout << "Hatalý iþlem seçtiniz.";
+	}
+	}
+}
+*/
