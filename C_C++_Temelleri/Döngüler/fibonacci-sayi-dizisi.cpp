08d7f8d515559d1ffabd7d40fdd1f5701a5ad2cc
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-19 15:59:48 +0300

    Fibonacci Sayı Dizisi

---
+++ C_C++_Temelleri/Döngüler/fibonacci-sayi-dizisi.cpp
@@ -0,0 +1,22 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int i, sayi1 = 1, sayi2 = 1, fibonacciSayisi = 0;
+
+	cout << "Fibonacci sayý dizisinin ilk 20 elemaný: " << endl;
+	cout << sayi1 << " " << sayi2;
+	
+	for (i = 1; i <= 18; i++)
+	{
+		fibonacciSayisi = sayi1 + sayi2;
+		cout << " " << fibonacciSayisi;
+
+		sayi1 = sayi2;
+		sayi2 = fibonacciSayisi;
+	}
+}
+*/
