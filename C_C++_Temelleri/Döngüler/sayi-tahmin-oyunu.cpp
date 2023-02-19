6d9c4376129f192a4cf72ed86b01af739767dde2
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-19 20:37:18 +0300

    Sayı Tahmin Oyunu

---
+++ C_C++_Temelleri/Döngüler/sayi-tahmin-oyunu.cpp
@@ -0,0 +1,36 @@
+/*
+#include <iostream>
+// #include <cstdlib>
+// #include <locale>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int bulunacakSayi, tahmin, denemeSayisi = 0;
+
+	srand(time(NULL));
+	bulunacakSayi = rand() % 100 + 1;
+
+	while (1)
+	{	
+		denemeSayisi++;
+		cout << "Tahmininizi giriniz: ";
+		cin >> tahmin;
+
+		if (tahmin == bulunacakSayi)
+		{
+			cout << "Tebrikler " << denemeSayisi << " hakta bildiniz. Bulunacak sayý " << bulunacakSayi << " idi.";
+			break;
+		}
+		else if (tahmin > bulunacakSayi)
+		{
+			cout << "Sayýnýzý azaltýnýz.";
+		}
+		else
+		{
+			cout << "Sayýnýzý artýrýnýz.";
+		}	
+	}
+}
+*/
