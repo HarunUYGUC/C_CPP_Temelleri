30ad6cb14d017c6f899b21d4988cb1d57f8c6137
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-19 16:15:33 +0300

    While Döngüsü

---
+++ C_C++_Temelleri/Döngüler/while-dongusu.cpp
@@ -0,0 +1,42 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		cout << i << endl;
+		i++;
+	}
+}
+*/
+
+// 0 girilene kadarki sayýlarýn toplamýný bulan program.
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	setlocale(LC_ALL, "Turkish");
+	int i = 1, sayi, toplam = 0;
+
+	cout << "0 girene kadarki sayýlarýn toplamýný bulabilirsiniz." << endl;
+	cout << "Bir sayý giriniz: ";
+	cin >> sayi;
+
+	while (sayi != 0)
+	{
+		toplam += sayi;
+
+		cout << "Bir sayý giriniz: ";
+		cin >> sayi;
+	}
+
+	cout << "Sayýlarýn toplamý = " << toplam;
+}
+*/
