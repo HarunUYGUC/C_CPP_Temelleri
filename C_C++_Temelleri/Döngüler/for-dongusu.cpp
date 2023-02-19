705505f447345d1f396ac6994851e1e4477f64f6
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-19 13:49:22 +0300

    For Döngüsü

---
+++ C_C++_Temelleri/Döngüler/for-dongusu.cpp
@@ -0,0 +1,62 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sayac;
+
+	for (sayac = 1; sayac <= 10; sayac++)
+	{
+		cout << sayac << endl;
+	}
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sayac;
+
+	for (sayac = 1; sayac <= 10; sayac += 2)
+	{
+		cout << sayac << endl;
+	}
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sayac;
+
+	for (sayac = 10; sayac >= 1; sayac -= 2)
+	{
+		cout << sayac << endl;
+	}
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sayac, toplam = 0;
+
+	for (sayac = 1; sayac <= 10; sayac++)
+	{
+		toplam += sayac;
+		cout << sayac << "'a kadarki sayýlarýn toplamý = " << toplam << endl;
+	}
+	
+	cout << "1-10 arasý sayýlarýn toplamý = " << toplam;
+}
+*/
---
+++ C_C++_Temelleri/Döngüler/for-ornekleri.cpp
@@ -0,0 +1,45 @@
+// Kullanýcýdan alýnan 10 sayýnýn toplamýný veren program.
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int i, sayi, toplam = 0;
+
+	for (i = 1; i <= 10; i++)
+	{
+		cout << i << ". sayýyý giriniz: ";
+		cin >> sayi;
+
+		toplam += sayi;
+	}
+	
+	cout << "Girdiðiniz sayýlarýn toplamý = " << toplam;
+}
+*/
+
+// Kullanýcýdan alýnan sayýnýn faktöriyelini hesaplayan program.
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	setlocale(LC_ALL, "Turkish");
+	int i, sayi, faktoriyel = 1;
+
+	cout << "Faktöriyelini hesaplamak istediðiniz sayýyý giriniz: ";
+	cin >> sayi;
+
+	for (i = 1; i <= sayi; i++)
+	{
+		faktoriyel *= i;
+	}
+
+	cout << sayi << " sayýsýnýn faktöriyeli = " << faktoriyel;
+}
+*/
