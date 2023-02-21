01f6fec3735d8f06f82b5d773013b74588926e71
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-21 16:52:28 +0300

    Çiftlerin Toplamı

---
+++ C_C++_Temelleri/Döngüler/cift-sayilarin-toplami.cpp
@@ -0,0 +1,23 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	setlocale(LC_ALL, "Turkish");
+	int i = 1, sayi, ciftlerinToplami = 0;
+
+	for (i; i <= 10; i++)
+	{
+		cout << i << ". sayýyý giriniz: ";
+		cin >> sayi;
+
+		if (sayi % 2 == 0)
+		{
+			ciftlerinToplami += sayi;
+		}
+	}
+
+	cout << "Çift sayýlarýn toplamý = " << ciftlerinToplami;
+}
+*/
