eebb54177bb549fbe06bfe5e4bc492bb06dff0ca
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-21 18:27:49 +0300

    Asal Sayı Mı?

---
+++ C_C++_Temelleri/Döngüler/asal-sayi-kontrolu.cpp
@@ -0,0 +1,28 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int sayi, bolen, bayrak = true;
+
+	cout << "Pozitif bir tam sayý giriniz: ";
+	cin >> sayi;
+
+	for (bolen = 2; bolen <= (sayi / 2); bolen++)
+	{
+		if (sayi % bolen == 0)
+		{	
+			cout << sayi << " asal sayý deðildir.";
+			bayrak = false;
+			break;
+		}
+	}
+
+	if (bayrak == true)
+	{
+		cout << sayi << " asal sayýdýr";
+	}
+}
+*/
