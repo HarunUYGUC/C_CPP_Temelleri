c19b00ee93d8241fad21de0c52e8c687c648f73e
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-21 15:44:52 +0300

    Asal Çarpan Bulma

---
+++ C_C++_Temelleri/Döngüler/asal-carpan-bulma.cpp
@@ -0,0 +1,26 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	setlocale(LC_ALL, "Turkish");
+	int sayi, asalCarpan = 2;
+
+	cout << "Asal çarpanlarýný bulmak istediðiniz bir tam sayýyý giriniz: ";
+	cin >> sayi;
+
+	while (sayi > 1)
+	{
+		if (sayi % asalCarpan == 0)
+		{
+			cout << asalCarpan << endl;
+			sayi /= asalCarpan;
+		}
+		else
+		{
+			asalCarpan++;
+		}
+	}
+}
+*/
