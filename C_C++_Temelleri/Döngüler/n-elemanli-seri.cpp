ef707cf12e2104e22ff0d79545841d8d0166963e
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-22 15:33:01 +0300

    n Elemanlı Seri

---
+++ C_C++_Temelleri/Döngüler/n-elemanli-seri.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/*
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int sayi, i;
+	float toplam = 0;
+
+	cout << "Pozitif bir tam sayý giriniz: ";
+	cin >> sayi;
+
+	for (i = 1; i <= sayi; i++)
+	{
+		toplam += 1.0 / i;
+	}
+
+	cout << "1 + 1/2 + 1/3 + ...... + 1/" << sayi << " = " << toplam;
+}
+*/
