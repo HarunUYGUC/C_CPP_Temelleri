63aad60f3f83038df2ac145321459824390f7bd3
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-23 11:38:31 +0300

    Parametre Almayan Fonksiyonlar

---
+++ C_C++_Temelleri/Fonksiyonlar/fonksiyonlar.cpp
@@ -0,0 +1,48 @@
+/*
+
+FONKSÝYONLAR:
+-- Belirli bir iþlevi yerine getirmek üzere ana programdan baðýmsýz kod bloklarýna fonksiyon denir.
+-- Ana program tarafýndan çaðrýlarak kodlarýn tekrar tekrar kullanýlmasýna imkan saðlar.
+-- Kod tekrarýný önleyerek hata tespitini kolaylaþtýrýr.
+
+Fonksiyonlarý 4 Baþlýk Altýnda Ýnceleyeceðiz:
+1- Parametre Almayan
+2- Parametre Alan
+3- Geriye Deðer Döndürmeyen
+4- Geriye Deðer Döndüren
+
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+void mesaj()
+{
+	cout << "Fonksiyonunuz çalýþtý.";
+}
+
+int main()
+{	
+	setlocale(LC_ALL, "Turkish");
+	mesaj();
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+void mesaj();
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	mesaj();
+}
+
+void mesaj()
+{
+	cout << "Fonksiyonunuz çalýþtý.";
+}
+*/
