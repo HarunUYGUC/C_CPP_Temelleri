5a60077bcf2bd72eb29af3e6751f7619620273d8
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-23 12:09:57 +0300

    Parametre Alan ve Değer Döndürmeyen Fonksiyonlar

---
+++ C_C++_Temelleri/Fonksiyonlar/fonksiyonlar-2.cpp
@@ -0,0 +1,19 @@
+/*
+#include <iostream>
+using namespace std;
+
+void topla(int sayi1, int sayi2)
+{
+	cout << sayi1 << " + " << sayi2 << " = " << sayi1 + sayi2 << endl;
+}
+
+
+int main()
+{
+	int a = 15, b = 20, c = 7, d = 12;
+
+	topla(a, b);
+	topla(c, d);
+	topla(a, d);
+}
+*/
