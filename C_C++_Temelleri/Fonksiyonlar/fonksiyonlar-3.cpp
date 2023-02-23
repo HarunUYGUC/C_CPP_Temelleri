01115e44e1b14a8e43e10d4086eac2705164a14b
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-23 12:36:35 +0300

    Parametre Alan ve Değer Döndüren Fonksiyonlar

---
+++ C_C++_Temelleri/Fonksiyonlar/fonksiyonlar-3.cpp
@@ -0,0 +1,28 @@
+// (5! + 7!) / (9! - 6!) iþlemini yapan programý yazýnýz.
+
+/*
+#include <iostream>
+using namespace std;
+
+int faktoriyel(int sayi)
+{
+	int i, fak = 1;
+
+	for (i = 1; i <= sayi; i++)
+	{
+		fak *= i;
+	}
+
+	return fak;
+}
+
+int main()
+{
+	int a = 5, b = 7, c = 9, d = 6;
+	double sonuc;
+
+	sonuc = double(faktoriyel(a) + faktoriyel(b)) / (faktoriyel(c) - faktoriyel(d));
+	
+	cout << "(5! + 7!) / (9! - 6!) iþlemininin sonucu = " << sonuc;
+}
+*/
