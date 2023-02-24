27c4f8c2a3ee485dfe2c2344d54149616dcee162
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-24 18:44:03 +0300

    Çemberin Çevresi ve Alanı

---
+++ C_C++_Temelleri/Fonksiyonlar/ornek-2.cpp
@@ -0,0 +1,27 @@
+/*
+#include <iostream>
+using namespace std;
+#define PI 3.14
+
+double cevre(double r)
+{
+	return 2 * PI * r;
+}
+
+double alan(double r)
+{
+	return PI * r * r;
+}
+
+int main()
+{	
+	setlocale(LC_ALL, "Turkish");
+	double r;
+
+	cout << "Yarýçapý giriniz: ";
+	cin >> r;
+
+	cout << "Çemberin çevresi = " << cevre(r) << endl;
+	cout << "Çemberin alaný = " << alan(r);
+}
+*/
