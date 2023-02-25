44d71029555cadec42a2b9a88810f93055f6a96c
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-25 22:19:55 +0300

    Aşırı Yüklenme & Template Kullanımı

---
+++ C_C++_Temelleri/Fonksiyonlar/asiri-yuklenme.cpp
@@ -0,0 +1,43 @@
+/*
+#include <iostream>
+using namespace std;
+
+int guncelle(int x)
+{
+	return ++x;
+}
+
+double guncelle(double x)
+{
+	return ++x;
+}
+
+int main()
+{
+	int a = 5;
+	double b = 10.3;
+
+	cout << guncelle(a) << endl;
+	cout << guncelle(b);
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+template <typename T>
+
+T guncelle(T x)
+{
+	return ++x;
+}
+
+int main()
+{
+	int a = 5;
+	double b = 10.3;
+
+	cout << guncelle(a) << endl;
+	cout << guncelle(b);
+}
+*/
