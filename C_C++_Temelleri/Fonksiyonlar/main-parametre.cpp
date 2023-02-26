7a9ef95dd21592f5ca8c8b2f28de5161f5806c3e
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-26 13:30:17 +0300

    Parametre Alan main()

---
+++ C_C++_Temelleri/Fonksiyonlar/main-parametre.cpp
@@ -0,0 +1,46 @@
+/*
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	setlocale(LC_ALL, "Turkish");
+	int a, b;
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	cout << a + b;
+
+	return 0;
+}
+*/
+
+/*
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Turkish");
+
+	cout << "Merhaba!!";
+
+	return 0;
+}
+*/
+
+/*
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Turkish");
+
+	cout << "Merhaba!!";
+}
+*/
