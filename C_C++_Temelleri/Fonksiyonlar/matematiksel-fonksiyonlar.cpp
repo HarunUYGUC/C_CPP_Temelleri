46be570207a3bfca92765855f84bb2128c03838f
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-28 16:06:21 +0300

    Matematiksel Fonksiyonlar

---
+++ C_C++_Temelleri/Fonksiyonlar/matematiksel-fonksiyonlar.cpp
@@ -0,0 +1,43 @@
+/*
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	double a = 23.51, c = 23.49;
+	int b = -15;
+
+	// fabs() fonksiyonu mutlak deðerin sonucunu bulmak için kullanýlýr. f, fonksiyon; abs, absolute yani mutlak demek.
+	cout << "fabs(): " << fabs(b) << endl;
+
+	// fmod(x, y) fonksiyonu mod alma iþlemini yapar. % sembolü ile ayný iþlevi görür.
+	cout << "fmod(x, y): " << fmod(20, 6) << endl;
+
+	// ceil() fonksiyonu ondalýklý sayýyý bir üst tam sayýya dönüþtürür. ceil, tavan demek.
+	cout << "ceil(): " << ceil(a) << endl;
+
+	// floor() fonksiyonu ondalýklý sayýyý bir alt tam sayýya dönüþtürür. floor, zemin demek.
+	cout << "floor(): " << floor(a) << endl;
+
+	// round() fonksiyonu ondalýklý sayýyý en yakýn tam sayýya yuvarlar. round, yuvarlamak demek.
+	cout << "round(): " << round(a) << endl;
+	cout << "round(): " << round(c) << endl;
+
+	// cos(), sin(), tan(), acos(), asin(), atan() trigonometrik fonksiyonlarý radyan cinsinden girilen deðerlerin karþýlýðýný verir. 
+	// a demek arc yani ters fonksiyon demek.
+	cout << "cos(): " << cos(0) << endl;
+	cout << "sin(): " << sin(0) << endl;
+
+	// max(x, y) ve min(x, y) fonksiyonlarý girilen sayýlardan büyük ve küçük olaný bulmak için kullanýlýr.
+	cout << "max(): " << max(36, 25) << endl;
+	cout << "min(): " << min(36, 25) << endl;
+
+	// pow(taban, üst) fonksiyonu üstlü ifadeleri hesaplamak için kullanýlýr. pow; güç, kuvvet demek. Çünkü 2'nin 3. kuvveti þeklinde de söyleyebiliyoruz.
+	cout << "pow(taban, üst): " << pow(2, 5) << endl;
+
+	// sqrt() fonksiyonu karekök hesaplamak için kullanýlýr. square root, karekök demek.
+	cout << "sqrt(): " << sqrt(36) << endl;
+}
+*/
