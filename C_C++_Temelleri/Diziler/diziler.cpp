6a813cf29d2442fd6b1547d1fe396f5450b1b66e
Harun Uyguç <harun.uyguc@hotmail.com>
2023-03-04 19:15:51 +0300

    Diziler

---
+++ C_C++_Temelleri/Diziler/diziler.cpp
@@ -0,0 +1,27 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int dizi1[10];
+	int dizi2[] = { 90, 80, 70, 55, 72 };
+	int dizi3[5] = { 90, 80, 70, 55, 72 };
+	int dizi4[10] = { 90, 80, 70, 55, 72 };
+
+
+	cout << dizi2[0] << endl;
+	cout << dizi2[1] << endl;
+
+	printf("%d \n", dizi2[3]);
+
+	dizi1[0] = 1;
+	dizi1[1] = 2;
+	dizi1[2] = 3;
+
+	cout << dizi1[0] << endl;
+
+	cout << dizi4[5];
+}
+*/
