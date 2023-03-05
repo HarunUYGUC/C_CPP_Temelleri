6ed7faca50feec8549a1cb1fc430b42fe202cef9
Harun Uyguç <harun.uyguc@hotmail.com>
2023-03-05 21:45:09 +0300

    String Dizisi

---
+++ C_C++_Temelleri/Diziler/char-dizisi.cpp
@@ -0,0 +1,80 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string metin = "Biliþim";
+	
+	cout << metin;
+
+	return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string ogrenciler[] = { "Ali", "Veli", "Mehmet" };
+
+	cout << ogrenciler[0];
+
+	return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string ogrenciler[] = { "Ali", "Veli", "Mehmet" };
+
+	cout << ogrenciler[0][0] << endl;
+
+	return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string ogrenciler[] = { "Ali", "Veli", "Mehmet" };
+
+	for (int i = 0; i < 3; i++)
+		cout << ogrenciler[i] << endl;
+
+	return 0;
+}
+*/
+
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string isimler[10];
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "Ýsim, soyisim giriniz: ";
+		getline(cin, isimler[i]);
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << isimler[i] << endl;
+	}
+
+	return 0;
+}
+*/
