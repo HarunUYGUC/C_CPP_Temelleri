ceeaf8c50b9be2eca031b4338748a5d3d40e9055
Harun Uyguç <harun.uyguc@hotmail.com>
2023-03-08 23:12:29 +0300

    Fonksiyon, Parametre, Dizi

---
+++ C_C++_Temelleri/Diziler/fonk-param-dizi.cpp
@@ -0,0 +1,26 @@
+/*
+#include <iostream>
+using namespace std;
+
+int toplam(int sayi[], int boyut)
+{
+	int sonuc = 0;
+
+	for (int i = 0; i < boyut; i++)
+	{
+		sonuc += sayi[i];
+	}
+
+	return sonuc;
+}
+
+int maint()
+{
+	setlocale(LC_ALL, "Turkish");
+	int sayilar[] = { 55, 25, 96, 72, 78, 28 };
+
+	cout << "Sonuç = " << toplam(sayilar, 6);
+	
+	return 0;
+}
+*/
