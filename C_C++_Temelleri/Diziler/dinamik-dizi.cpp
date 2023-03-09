461306ac08c1f91573868457a42962e85d332000
Harun Uyguç <harun.uyguc@hotmail.com>
2023-03-09 16:08:45 +0300

    Dinamik Dizi

--- C_C++_Temelleri/Diziler/dinamik-dizi.cpp
+++ C_C++_Temelleri/Diziler/dinamik-dizi.cpp
@@ -1,3 +1,16 @@
+// Diðer örnekte, ChatGPT sayesinde :D pointer kullanarak hatayý düzelttim. Aþaðýdaki þekilde bir soru sordum ve oldu.
+
+/*
+I want to determine the size of the list according to the number I will get from the user, but I am getting an error.
+
+int piece;
+
+cout << "How many numbers do you want to enter and then sum?: ";
+cin >> piece;
+
+int numbers[piece];
+*/
+
 /*
 #include <iostream>
 using namespace std;
@@ -31,6 +44,44 @@ int main()
 	}
 
 	cout << sayilar[adet - 1] << " = " << toplam;
+
+	return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int adet, toplam = 0;
+
+	cout << "Kaç tane sayý girip toplamak istiyorsunuz?: ";
+	cin >> adet;
+
+	int* sayilar = new int[adet];
+
+	for (int i = 0; i < adet; i++)
+	{
+		cout << i + 1 << ". sayýyý giriniz: ";
+		cin >> sayilar[i];
+	}
+
+	for (int i = 0; i < adet; i++)
+	{
+		toplam += sayilar[i];
+	}
+
+	for (int i = 0; i < (adet - 1); i++)
+	{
+		cout << sayilar[i] << " + ";
+	}
+
+	cout << sayilar[adet - 1] << " = " << toplam;
+
+	delete[] sayilar;
 
 	return 0;
 }
