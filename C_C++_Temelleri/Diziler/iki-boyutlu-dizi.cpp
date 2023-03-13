31503fd29bd2e56b07153aadb99d8cde97b98d80
Harun Uyguç <harun.uyguc@hotmail.com>
2023-03-13 16:24:06 +0300

    İki Boyutlu Dizi

---
+++ C_C++_Temelleri/Diziler/iki-boyutlu-dizi.cpp
@@ -0,0 +1,27 @@
+/*
+// dizi[satýr][sütun]
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int sayilar[3][5] = { {56, 78, 65, 90, 85}, {45, 56, 45, 75, 80}, {90, 95, 70, 85, 95} };
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			cout << sayilar[i][j] << "\t";
+		}
+
+		cout << endl;
+	}
+	
+	return 0;
+}
+*/
+
+// int sayilar[3][5] = { 56, 78, 65, 90, 85, 45, 56, 45, 75, 80, 90, 95, 70, 85, 95 };
+// Bu þekilde de yazarsak yine ayný sonucu alýrýz.
