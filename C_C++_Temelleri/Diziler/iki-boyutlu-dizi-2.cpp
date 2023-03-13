e78988ba7245d9556c65e810dc1b0124de256670
Harun Uyguç <harun.uyguc@hotmail.com>
2023-03-13 16:36:37 +0300

    İki Boyutlu Dizi

---
+++ C_C++_Temelleri/Diziler/iki-boyutlu-dizi-2.cpp
@@ -0,0 +1,31 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int matris[3][3];
+	
+	for (int satir = 0; satir < 3; satir++)
+	{
+		for (int sutun = 0; sutun < 3; sutun++)
+		{
+			cout << satir + 1 << ". Satýr, " << sutun + 1 << ". Sütundaki Elemaný Giriniz: ";
+			cin >> matris[satir][sutun];
+		}
+	}
+
+	for (int sat = 0; sat < 3; sat++)
+	{
+		for (int sut = 0; sut < 3; sut++)
+		{
+			cout << matris[sat][sut] << "\t";
+		}
+
+		cout << endl;
+	}
+
+	return 0;
+}
+*/
