7796f845379e522d78b18c50923d1ce2aaa38218
Harun Uyguç <harun.uyguc@hotmail.com>
2023-03-14 21:29:46 +0300

    Matrisin Transpozu

---
+++ C_C++_Temelleri/Diziler/matrisin-devrigi.cpp
@@ -0,0 +1,31 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int matrisA[2][3] = { {1, 2, 3}, {4, 5, 6} };
+	int matrisB[3][2];
+
+	for (int satir = 0; satir < 2; satir++)
+	{
+		for (int sutun = 0; sutun < 3; sutun++)
+		{
+			matrisB[sutun][satir] = matrisA[satir][sutun];
+		}
+	}
+
+	for (int sat = 0; sat < 3; sat++)
+	{
+		for (int sut = 0; sut < 2; sut++)
+		{
+			cout << matrisB[sat][sut] << "\t";
+		}
+
+		cout << endl;
+	}
+	
+	return 0;
+}
+*/
