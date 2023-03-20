0503217206e47e71614f2487a77afa0558e41ff6
Harun Uyguç <harun.uyguc@hotmail.com>
2023-03-20 14:13:40 +0300

    Update

--- C_C++_Temelleri/Koşul İfadeleri/switch-case.cpp
+++ C_C++_Temelleri/Koşul İfadeleri/switch-case.cpp
@@ -84,3 +84,39 @@ int main()
 	}
 }
 */
+
+
+// Üni. dersinden öðrendiðim kurallar...
+/*
+- {} kullanmadan da switch-case yapýsýndaki case durumlarýný farklý satýrlarýn arasýnda ; olacak þekilde 
+tek satýrda yazabiliriz. Aslýnda bunu sadece switch-case durumu için deðil genel durumlarda da kullanabiliriz
+ama çok kullanýþlý olacaðýný düþünmüyorum. Switch-case de ise duruma göre gayet kullanýþlý olabilir.
+
+- Üstteki case durumu gerçekleþse de gerçekleþmese de break komutunu içermiyorsa break komutu olan satýra kadarki
+kodlar çalýþýr.
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+	int s;
+
+	printf("Bir tam sayý giriniz: ");
+	scanf_s("%d", &s);
+
+	switch (s)
+	{
+	case 3:
+	case 5:
+	case 8: printf("Kazandý! \n"); printf("Deneme Metni: Harikasýn. :D"); break;
+	case 1:
+	case 4:
+	case 7:
+	case 9: printf("Kaybetti!"); break;
+	default: printf("Berabere");
+	}
+
+	return 0;
+}
+*/
