413f51d55c84def204eb2b6fde65f21d5d495f2d
Harun Uyguç <harun.uyguc@hotmail.com>
2023-03-20 15:29:39 +0300

    Pointers (İşaretçiler)

---
+++ C_C++_Temelleri/Pointers (İşaretçiler)/pointers.cpp
@@ -0,0 +1,43 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int a = 15;
+	int *pt;
+
+	cout << "a deðiþkeninin deðeri: " << a << endl;
+	pt = &a;
+	cout << "pointer'ýn deðeri (yani a deðiþkeninin adresi): " << pt << endl;
+	cout << "pointer'ýn iþaret ettiði adresteki deðer: " << *pt << endl;
+
+	*pt = 25;
+	cout << "a deðiþkeninin yeni deðeri: " << a << endl;
+
+	cout << "pointer'ýn adresi: " << &pt;
+	
+	return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int a = 15;
+	int* pt;
+
+	cout << "a deðiþkeninin deðeri: " << a << endl;
+	pt = &a;
+	printf("pointer'ýn deðeri (yani a deðiþkeninin adresi): %p \n", pt);
+	printf("pointer'ýn adresi: %p \n", &pt);
+	printf("pointer'ýn iþaret ettiði adresteki deðer: %d", *pt);
+
+	return 0;
+}
+*/
