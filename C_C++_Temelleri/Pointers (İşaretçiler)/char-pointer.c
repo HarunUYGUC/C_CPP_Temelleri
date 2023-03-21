77b51bc87c133e81f54983916ee2b80c351b5b2f
Harun Uyguç <harun.uyguc@hotmail.com>
2023-03-21 17:25:37 +0300

    Char Pointer

---
+++ C_C++_Temelleri/Pointers (İşaretçiler)/char-pointer.c
@@ -0,0 +1,49 @@
+/*
+#include <stdio.h>
+
+int main()
+{
+	char ad[] = "Ali";
+
+	printf("%s \n", ad);
+	printf("Byte olarak boyutu: %d \n", sizeof(ad));
+	printf("Karakter sayýsý: %d \n", strlen(ad));
+	
+	return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main()
+{
+	char ad[] = "Ali";
+	char* pt;
+
+	pt = ad;
+	printf("%s \n", pt);
+	printf("%c \n", pt[0]);
+	printf("%c \n", *(pt + 1));
+
+	pt++;
+	printf("%c", *pt);
+
+	return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main()
+{
+	char ad[] = "Ali";
+	char* pt = ad;
+
+	pt[0] = 'E';
+	printf("%s", pt);
+
+	return 0;
+}
+*/
