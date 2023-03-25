b3e9282f00962ce4f3aad36210f67c54a92f3b24
Harun Uyguç <harun.uyguc@hotmail.com>
2023-03-25 11:51:21 +0300

    Pointer Parametre

---
+++ C_C++_Temelleri/Pointers (İşaretçiler)/pointer-parametre-2.cpp
@@ -0,0 +1,27 @@
+/*
+#include <iostream>
+using namespace std;
+
+int topla(int* pt, int boyut)
+{
+	int toplam = 0;
+
+	for (int i = 0; i < boyut; i++)
+	{
+		printf("%x : %d \n", (pt + i), *(pt + i));
+		toplam += *(pt + i);
+	}
+
+	return toplam;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int sayilar[] = { 15, 25, 63, 89, 24 };
+	
+	cout << "Dizideki sayýlarýn toplamý: " << topla(sayilar, 5);
+
+	return 0;
+}
+*/
