4b5427d3914aaa17bb6be23ba115414b846369af
Harun Uyguç <harun.uyguc@hotmail.com>
2023-03-25 13:15:11 +0300

    Pointers

---
+++ C_C++_Temelleri/Pointers (İşaretçiler)/pointers.c
@@ -0,0 +1,17 @@
+/*
+#include <stdio.h>
+
+int main()
+{
+	int a = 15;
+	int* pt;
+
+	printf("a deðiþkeninin deðeri: %d \n", a);
+	pt = &a;
+	printf("pointer'ýn deðeri (yani a deðiþkeninin adresi): %p \n", pt);
+	printf("pointer'ýn adresi: %p \n", &pt);
+	printf("pointer'ýn iþaret ettiði adresteki deðer: %d", *pt);
+
+	return 0;
+}
+*/
