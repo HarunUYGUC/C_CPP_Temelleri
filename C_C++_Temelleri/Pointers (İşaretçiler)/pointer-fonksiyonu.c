dc94598528acb7dede4f77efdcc9394616e510cd
Harun Uyguç <harun.uyguc@hotmail.com>
2023-03-25 12:32:43 +0300

    Pointer Fonksiyonu

---
+++ C_C++_Temelleri/Pointers (İşaretçiler)/pointer-fonksiyonu.c
@@ -0,0 +1,29 @@
+/*
+#include <stdio.h>
+
+char* ara(char* p, char kr)
+{
+	while (*p != '\0')
+	{
+		if (*p == kr)
+		{
+			return p;
+		}
+
+		p++;
+	}
+}
+
+int main()
+{
+	char metin[] = "Biliþim Dünyasý";
+	char* pr = ara(metin, 'm');
+	
+	if (pr)
+		printf("%c karakteri %x adresinde bulundu.", *pr, pr);
+	else
+		printf("Karakteriniz bulunamadý.");
+
+	return 0;
+}
+*/
