d0bdcc958db99af84b2cf8948f6022880a535076
Harun Uyguç <harun.uyguc@hotmail.com>
2023-03-30 12:55:46 +0300

    String Tersten Yazdır

---
+++ C_C++_Temelleri/Pointers (İşaretçiler)/string-tersten-yazdir.cpp
@@ -0,0 +1,30 @@
+/*
+#include <iostream>
+using namespace std;
+
+char* tersptr;
+
+void tersYaz(char* cp)
+{
+	int uzunluk = strlen(cp) - 1;
+
+	tersptr = (char*)calloc(uzunluk, sizeof(char));
+
+	for (int i = uzunluk; i >= 0; i--)
+	{
+		tersptr[uzunluk - i] = cp[i];
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	char* metin;
+
+	metin = "Biliþim Dünyasý C ve C++ Dersleri";
+	tersYaz(metin);
+	cout << tersptr;
+
+	return 0;
+}
+*/
