595fb701d6bb4ffef96600ba71294ca5267d4f00
Harun Uyguç <harun.uyguc@hotmail.com>
2023-04-02 22:45:50 +0300

    Vektör Metodları

---
+++ C_C++_Temelleri/Vektörler/vektorler-2.cpp
@@ -0,0 +1,50 @@
+/*
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	vector <int> vek1;
+
+	vek1.push_back(10);
+
+	cout << "Eleman sayýsý: " << vek1.size() << endl;
+	cout << "Kapasite: " << vek1.capacity() << endl;
+	
+	vek1.push_back(10);
+
+	cout << "Eleman sayýsý: " << vek1.size() << endl;
+	cout << "Kapasite: " << vek1.capacity() << endl;
+
+	return 0;
+}
+*/
+
+/*
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	vector <int> vek1;
+
+	for (int i = 0; i < 64; i++)
+	{
+		vek1.push_back(10);
+
+		cout << "Eleman sayýsý: " << vek1.size() << endl;
+		cout << "Kapasite: " << vek1.capacity() << endl;
+	}
+
+	vek1.shrink_to_fit();
+
+	cout << "Eleman sayýsý: " << vek1.size() << endl;
+	cout << "Kapasite: " << vek1.capacity() << endl;
+
+	return 0;
+}
+*/
