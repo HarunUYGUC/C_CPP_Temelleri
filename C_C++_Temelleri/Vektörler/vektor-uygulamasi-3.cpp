74e454a6a473f0f3e660c4b727157cb388047420
Harun Uyguç <harun.uyguc@hotmail.com>
2023-04-16 13:03:28 +0300

    Fonksiyona Referans Tipi Vektör Gönder

---
+++ C_C++_Temelleri/Vektörler/vektor-uygulamasi-3.cpp
@@ -0,0 +1,33 @@
+/*
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void guncelle(vector <int> &rakamlar)
+{
+	for (int i = 0; i < rakamlar.size(); i++)
+		rakamlar[i] *= 10;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	vector <int> sayilar = { 1, 2, 3, 4, 5, 6, 7, 8 };
+	
+	cout << "Vektörün Ýlk Hali: " << endl;
+	for (int sayi : sayilar)
+	{
+		cout << sayi << endl;
+	}
+
+	guncelle(sayilar);
+
+	cout << "Vektörün Son Hali: " << endl;
+	for (int sayi : sayilar)
+	{
+		cout << sayi << endl;
+	}
+
+	return 0;
+}
+*/
