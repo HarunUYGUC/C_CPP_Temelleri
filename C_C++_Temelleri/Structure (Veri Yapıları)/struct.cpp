6c3ad9e0ce60e8068f76e4d4c58d04b10f7b0019
Harun Uyguç <harun.uyguc@hotmail.com>
2023-04-25 16:48:56 +0300

    Struct (Yapı)

---
+++ C_C++_Temelleri/Structure (Veri Yapıları)/struct.cpp
@@ -0,0 +1,37 @@
+/*
+#include <iostream>
+using namespace std;
+
+struct ogrenci
+{
+	int numara;
+	char ad[20];
+	char soyad[20];
+	double ortalama;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	struct ogrenci ogr;
+
+	cout << "Öðrencinin numarasýný giriniz: ";
+	cin >> ogr.numara;
+
+	cout << "Öðrencinin adýný giriniz: ";
+	cin >> ogr.ad;
+
+	cout << "Öðrencinin soyadýný giriniz: ";
+	cin >> ogr.soyad;
+
+	cout << "Öðrencinin ortalamasýný giriniz: ";
+	cin >> ogr.ortalama;
+
+	cout << endl;
+
+	cout << "Numara" << "\t" << "Ad" << "\t" << "Soyad" << "\t" << "Ortalama" << endl;
+	cout << ogr.numara << "\t" << ogr.ad << "\t" << ogr.soyad << "\t" << ogr.ortalama;
+
+	return 0;
+}
+*/
