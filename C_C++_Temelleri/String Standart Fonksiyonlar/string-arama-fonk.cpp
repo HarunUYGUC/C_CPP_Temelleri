b79e1bea9c7e411a6c2a33bc41beadc019d04f7b
Harun Uyguç <harun.uyguc@hotmail.com>
2023-05-20 15:21:25 +0300

    String Arama Fonksiyonları

---
+++ C_C++_Temelleri/String Standart Fonksiyonlar/string-arama-fonk.cpp
@@ -0,0 +1,54 @@
+/*
+
+strchr(char s1[], char c1) => c1 karakterinin s1 stringi içinde yer alýp almadýðýný kontrol eder. 
+Eðer yer alýyorsa pointer deðiþkenine yer aldýðý ilk noktanýn adresini döndürür.
+
+strrchr(char s1[], char c1) => c1 karakterini s1 string ifadesinin sonundan arama yapar.
+Eðer yer alýyorsa pointer deðiþkenine yer aldýðý ilk noktanýn adresini döndürür.
+
+strstr(char s1[], char s2[]) => s2 stringinde yer alan karakterlerden s1 stringinde yer alýp almadýðýný kontrol eder.
+Karakterlerin yer almasý durumunda pointer deðiþkenine ilk konum bilgisini atar.
+
+strcspn(char s1[], char s2[]) => bir karakter dizisi içindeki karakterlerin herhangi birinin diðer karakter dizisi
+içinde bulunduðu ilk yerin indeksini hesaplar.
+
+*/
+
+/*
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	//char str[] = "Bilisim Dunyasi Udemy Kursu";
+	//char* ptr;
+
+	//ptr = strchr(str, 'm');
+	//printf("%x \n", ptr);
+	//printf("%s", ptr);
+
+	//ptr = strrchr(str, 'm');
+	//printf("%x \n", ptr);
+	//printf("%s", ptr);
+
+	//ptr = strstr(str, "Dunyasi");
+	//printf("%x \n", ptr);
+	//printf("%s", ptr);
+
+	//ptr = strstr(str, "program");
+	//printf("%x \n", ptr);
+	//printf("%s", ptr);
+
+
+	//char str[] = "Bilisim Dunyasi Udemy Kursu";
+	//int konum;
+
+	//konum = strcspn(str, "Uum");
+	//printf("%d", konum);
+
+	return 0;
+}
+*/
